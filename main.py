c87643d7ba75d87e78f30e9633b47e9f67c5ea31
Kamlesh Jambani <66863611+kjambani@users.noreply.github.com>
2026-04-17 16:25:27 +0100

    model evaluation added

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import os
+import sys
 from mlopsProject.components import data_ingestion
+
+sys.stdout.reconfigure(encoding='utf-8')
+
+os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/kjambani/Mlops-end.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"]="kjambani"
+os.environ["MLFLOW_TRACKING_PASSWORD"]="41fd6ac15ed1729eecda790e1bd58ea6d7221984"
+
 from mlopsProject import logger
 from mlopsProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlopsProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlopsProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline  
 from mlopsProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from mlopsProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 
 
@@ -43,6 +53,16 @@ try:
     logger.info(f"stage {STAGE_Name}")
     model_trainer = ModelTrainerTrainingPipeline()
     model_trainer.main()
+    logger.info("Completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_Name = "Model Evaluation Stage"
+try:
+    logger.info(f"stage {STAGE_Name}")
+    model_evaluation = ModelEvaluationTrainingPipeline()
+    model_evaluation.main()
     logger.info("Completed")
 except Exception as e:
     logger.exception(e)
